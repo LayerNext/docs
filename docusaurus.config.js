e074b8b2a900d3ba7f67d419e8ca5903ec2d69c2
channa-zoomi <channa@zoomi.ca>
2025-12-08 11:36:11 +0530

    Revert "Merge branch 'main' into main"
    This reverts commit acda5920bfcd510c28f3d50a8ec905684110389f, reversing
    changes made to 10a9a6156e59f429821ceefc2306814dfb90c6c2.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -73,7 +73,6 @@ const config = {
     themeConfig:
         /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
         ({
-            metadata: [{ name: "robots", content: "noindex, nofollow" }],
             colorMode: {
                 defaultMode: "dark",
             },
